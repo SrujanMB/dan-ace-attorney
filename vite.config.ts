6d2fc1f6040ef2ea6a28ac5f930deaa08cd8cff3
Srujan MB <149997229+SrujanMB@users.noreply.github.com>
2026-05-13 19:33:10 +1200

    Add tailwindcss to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import tailwindcss from "@tailwindcss/vite";
 
 const PORT = Number(process.env.CLIENT_PORT) || 5000;
 const SOCKET_PORT = Number(process.env.SERVER_PORT) || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [basicSsl(), react()],
+  plugins: [basicSsl(), react(), tailwindcss()],
   server: {
     allowedHosts: ["ace.srujanmb.com"],
     port: PORT,
